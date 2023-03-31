72787b5dcb66eb407d6bde21fec4138c6d146d4d
frojoo <wndyd29605@gmail.com>
2023-03-31 14:05:15 +0900

    ABC

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import A from "./components/A";
+
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      <div className="bg-blue-300 w-full h-80">1</div>
-      <div className="bg-green-300 w-full h-80">2</div>
-      <div className="bg-purple-300 w-full h-80">3</div>
+      <A />
+      <B />
+      <C />
     </div>
   );
 };
---
+++ src/components/C.jsx
@@ -0,0 +1,16 @@
+const C = () => {
+  return (
+    <div className="bg-purple-300 w-full h-80">
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}3{/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+};
+
+export default C;
